b8e80e7a949bb314508571779c3cb099003ae0b4
bipon68 <bipon68@users.noreply.github.com>
2020-07-24 21:14:37 +0600

    search query working

--- src/App.js
+++ src/App.js
@@ -9,22 +9,23 @@ function App() {
 
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await Axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await Axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
       console.log(result.data);
 
       setItems(result.data);
       setIsLoading(false);
     }
     fetchItems()
-  }, [])
+  }, [query])
 
   return (
     <div className="container">
       <Header />
-      <Search />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharactersGrid isLoading={isLoading} items={items} />
     </div>
   );
--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 
-const Search = () => {
+const Search = ({getQuery}) => {
 
     const [text, setText] = useState('')
+
+    const onChange = (q) => {
+        setText(q)
+        getQuery(q)
+    }
 
     return (
         <section className='search'>
@@ -12,7 +17,8 @@ const Search = () => {
                     className='form-control' 
                     placeholder='Search characters'
                     value={text}
-                    onChange={(e) => setText(e.target.value)}
+                    onChange={(e) => onChange(e.target.value)}
+                    // onChange={(e) => setText(e.target.value)}
                     autoFocus
                     />
             </form>
